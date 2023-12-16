b4d7a82c9109019c30df75c109dbb1f3649b71f1
coconut9813 <96679779+coconut9813@users.noreply.github.com>
2023-12-16 18:46:52 +0300

    Update1348 script.js

--- script.js
+++ script.js
@@ -14,6 +14,33 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
-    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 2899', 'success')
+    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 2899', 'success')})
+}
+
+const alertTrigger2 = document.getElementById('liveAlertBtn2')
+if (alertTrigger2) {
+  alertTrigger2.addEventListener('click', () => {
+    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 1899', 'success')
+  })
+}
+
+const alertTrigger3 = document.getElementById('liveAlertBtn3')
+if (alertTrigger3) {
+  alertTrigger3.addEventListener('click', () => {
+    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 2999', 'success')
+  })
+}
+
+const alertTrigger4 = document.getElementById('liveAlertBtn4')
+if (alertTrigger4) {
+  alertTrigger4.addEventListener('click', () => {
+    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 3899', 'success')
+  })
+}
+
+const alertTrigger5 = document.getElementById('liveAlertBtn5')
+if (alertTrigger5) {
+  alertTrigger5.addEventListener('click', () => {
+    appendAlert('Отлично, товар: Майка для программиста в корзине; цена: 999', 'success')
   })
 }
